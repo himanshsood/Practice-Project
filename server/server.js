f8ce2dde6895b0a3aaff288b3216673e0270da2f
Himansh <himansh402.be22@chitkara.edu.in>
2024-10-23 11:29:44 +0530

    hbs template engine, CSR vs SSR

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const connectDb = require("./config/dbConnection")
-// const errorHandler = require("./middleware/errorHandler")
+const mongoose=require("mongoose")
+const errorHandler = require("./middleware/errorHandler")
 
 const cors = require("cors")
 
@@ -10,19 +11,85 @@ dotenv.config();
 
 
 
-connectDb()
+// connectDb()
+
+
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
-// app.use(errorHandler)
+app.set("view engine", "hbs");
+
+app.use(errorHandler)
+
+
+
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/HealthCare System', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const mySchema=new mongoose.Schema({
+    name:String,
+    age:Number
+})
+
+const User= mongoose.model('User',mySchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
 })
 
+app.get("/home",(req,res)=>{
+
+    res.render("home",{
+
+    })
+})
+
+app.get("/users",(req,res)=>{
+    res.render("users",{
+
+        people:[
+            {
+                username:"himansh",
+                age:20
+            },
+            {
+                username:"mahajan",
+                age:21
+            }
+        ]
+        
+        
+    })
+})
+
 
 app.listen(port, ()=>{
-    console.log("Server is running on port 5000")
+    console.log("Server is running on port 3001")
 })
